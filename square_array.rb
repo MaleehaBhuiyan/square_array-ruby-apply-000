cbba9ebd5c2bb92ec81101dd62430df0486ea3ee
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-22 22:29:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ numbers = [1,2,3]
 
 def square_array(numbers)
   squared_array = []
-  squared_array.each{|number| new_array.push(number*number)}
+  numbers.each{|number| new_array.push(number*number)}
   return square_array
 end
